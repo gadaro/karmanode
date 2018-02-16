2dd03ad8b2f79f417544d18980558eaaf6236494
Gabriel <gadaro@consultant.com>
2018-02-16 23:02:01 +0100

    Fixes

--- karmanode_setup.sh
+++ karmanode_setup.sh
@@ -17,22 +17,13 @@ function gather_user_info () {
 	echo "|--------------------------------------------------"
 	echo ""
 	read -p "  Karmanode genkey: " KARMANODE_GENKEY
-	read -p "  Karmanode outputs tx_hash: " KARMANODE_TX_HASH
-	read -p "  Karmanode outputs idx: " KARMANODE_IDX
 	read -p "  Karmanode user (Default ohmcoin): " KARMANODE_USER
+	read -s -p "  Karmanode password: " KARMANODE_PASS
 	read -p "  Karmanode rpc user: " KARMANODE_RPC_USER
 	read -p "  Karmanode rpc password: " KARMANODE_RPC_PASS
 
 	if [ -z "$KARMANODE_GENKEY" ]; then
 		echo "=> Information provided on Karmanode genkey is not correct."
-		ERRORS=true
-	fi
-	if [ -z "$KARMANODE_TX_HASH" ]; then
-		echo "=> Information provided on Karmanode outputs tx_hash is not correct."
-		ERRORS=true
-	fi
-	if [ -z "$KARMANODE_IDX" ]; then
-		echo "=> Information provided on Karmanode outputs idx is not correct."
 		ERRORS=true
 	fi
 	if [ -z "$KARMANODE_RPC_USER" ]; then
@@ -58,8 +49,6 @@ function check_info () {
 
 	echo ""
 	echo "Karmanode genkey: $KARMANODE_GENKEY"
-	echo "Karmanode outputs tx_hash: $KARMANODE_TX_HASH"
-	echo "Karmanode outputs idx: $KARMANODE_IDX"
 	echo "Karmanode user: $KARMANODE_USER"
 	echo "Karmanode rpc user: $KARMANODE_RPC_USER"
 	echo "Karmanode rpc password: $KARMANODE_RPC_PASS"
@@ -85,7 +74,7 @@ function system_upgrade () {
 function user_creation () {
 
 	useradd -m -U -s /bin/bash $KARMANODE_USER
-	passwd $KARMANODE_USER
+    echo $KARMANODE_USER:$KARMANODE_PASS | chpasswd
 	echo "Granting sudo privileges..."
 	adduser $KARMANODE_USER sudo
     echo "Granting make without password..."
@@ -134,8 +123,8 @@ function compile_ohm_daemon () {
     su -c "cd ~/ohmcoin; sudo make" $KARMANODE_USER
     su -c "cd ~/ohmcoin; sudo make install" $KARMANODE_USER
     su -c "mkdir ~/.ohmc" $KARMANODE_USER
-    su -c "echo \"rpcuser=$KARMANODE_RPC_USER\" > ~/.ohmc/ohmc.conf" $KARMANODE_USER
-    su -c "echo \"rpcpassword=$KARMANODE_RPC_PASS\" >> ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo rpcuser=$KARMANODE_RPC_USER > ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo rpcpassword=$KARMANODE_RPC_PASS >> ~/.ohmc/ohmc.conf" $KARMANODE_USER
 
 }
 
@@ -143,34 +132,93 @@ function installing_blockchain () {
 
     su -c "cd ~/.ohmc; wget http://www.ohmcoin.org/downloads/OHMC_Blockchain_snapshot.zip" $KARMANODE_USER
     su -c "cd ~/.ohmc; unzip OHMC_Blockchain_snapshot.zip" $KARMANODE_USER
-    su -c "cd ~/.ohmc; rm OHMC_Blockchain_snapshot.zip" $KARMANODE_USER
+    su -c "cd ~/.ohmc; rm OHMC_Blockchain_snapshot.zip; rm -fR __MACOSX; rm OHMC/.DS_Store" $KARMANODE_USER
+    su -c "cd ~/.ohmc; mv OHMC/* .; rm -fR OHMC" $KARMANODE_USER
+
+}
+
+function start_daemon () {
+
+    su -c "cd ~/.ohmc; ohmcd -daemon" $KARMANODE_USER
+
+}
+
+function waiting_sync () {
+
+    su -c "ohmc-cli getinfo | grep blocks" $KARMANODE_USE
+
+}
+
+function configure_daemon () {
+
+    su -c "echo rpcallowip=127.0.0.1 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo staking=1 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo server=1 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo listen=1 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo daemon=1 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo logtimestamps=1 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo maxconnections=256 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo karmanode=1 | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
+    su -c "echo karmanodeprivkey=$KARMANODE_GENKEY | tee -a ~/.ohmc/ohmc.conf" $KARMANODE_USER
 
 }
 
 gather_user_info
 check_info
 
+echo ""
 echo "--- Running system upgrade..."
+echo ""
 system_upgrade
 
+echo ""
 echo "--- Creating user $KARMANODE_USER..."
+echo ""
 user_creation
 
+echo ""
 echo "--- Disabling remote root login..."
+echo ""
 modify_sshd_config
 
+echo ""
 echo "--- Creating swap partition..."
+echo ""
 create_swap_space
 
+echo ""
 echo "--- Installing and configuring ohmc daemon..."
+echo ""
 compile_ohm_daemon
 
-echo "Installing blockchain snapshot..."
+echo ""
+echo "--- Installing blockchain snapshot..."
+echo ""
 installing_blockchain
 
-echo "Starting ohmc daemon..."
+echo ""
+echo "--- Starting ohmc daemon..."
+echo ""
+start_daemon
 
-#cd ~/.ohmc
-#ohmcd -daemon
+echo ""
+echo "--- Please wait daemon to get synced..."
+echo ""
+SYNCED=n
+while [ $SYNCED == "n" ]; do
+    waiting_sync
+    read -p "It is the daemon synced? [y/n]: " SYNCED
+done
+echo "Daemon synced"
 
-echo "Waiting to daemon to get synced..."
+echo ""
+echo "--- Applying following daemon configuration..."
+echo ""
+configure_daemon
+
+echo ""
+echo "--- Karmanode setup finished, system is going to be restarted in one minute"
+echo "--- Remember to restart the daemon with the following command on user $KARMANODE_USER"
+echo "--- cd ~/.ohmc; ohmcd -daemon"
+echo ""
+shutdown -r +1
